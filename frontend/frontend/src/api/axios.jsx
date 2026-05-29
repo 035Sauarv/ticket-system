3a8b89dd7932c087b7a9b9a4dd66eb60a1e00dec
saurav deshmane <sauravdeshmane095@gmail.com>
2026-05-29 06:10:20 +0530

    Updated frontend API URL to Elastic IP

--- frontend/frontend/src/api/axios.jsx
+++ frontend/frontend/src/api/axios.jsx
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://127.0.0.1:8000",
+    baseURL: "http://13.204.14.205:8000",
 });
 
 API.interceptors.request.use((config) => {
